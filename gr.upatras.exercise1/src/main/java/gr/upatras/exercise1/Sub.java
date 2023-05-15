ffeb51bfddb416e14c1b3605b7946ea46e38f559
up1072685 <up1072685@yk1-pc12.ece.upatras.gr>
2023-05-15 16:31:29 +0300

    added comments for Sub class

--- gr.upatras.exercise1/src/main/java/gr/upatras/exercise1/Sub.java
+++ gr.upatras.exercise1/src/main/java/gr/upatras/exercise1/Sub.java
@@ -2,6 +2,15 @@ package gr.upatras.exercise1;
 
 public class Sub {
 	public String mySub(int a, int b) {
+		/*
+		 * Developer: up1072685
+		 * Developer github name: FireCoder-N
+		 * 
+		 * This function takes 2 integers a, b as arguments 
+		 * and returns "POSITIVE" if a-b>=0
+		 * and "NEGATIVE" otherwise.
+		 * 
+		 */
 		String result;
 		if (a-b>=0) {
 			result = "POSITIVE";
@@ -13,6 +22,10 @@ public class Sub {
 	}
 
 	public static void main(String[] args) {
+		/*
+		 * The main function of the program. Used as example/test for the mySub method.
+		 * Tested with numbers a=10, b=12. => returned "NEGATIVE", because a-b = -2 < 0 
+		 */
 		Sub sub = new Sub();
 		String result = sub.mySub(10, 12);
 		System.out.println(result);
